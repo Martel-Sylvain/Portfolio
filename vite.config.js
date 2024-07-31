c96a57b12edcefd002c4c297b9c4c07500e23b31
sylvain2707666 <165642388+sylvain2707666@users.noreply.github.com>
2024-07-31 17:16:24 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/',  // Ajoutez cette ligne
   plugins: [react()],
   build: {
-    outDir: 'dist', // Dossier de sortie
-    rollupOptions: {
-      // Configuration supplémentaire pour Rollup si nécessaire
-    },
+    outDir: 'dist',
   },
 });
